ba67b244ce11a09d01d17fb349a6ff871c8339d7
SeonghwanNo-dev <seonghwangood01@gmail.com>
2026-05-01 17:33:34 +0900

    refactor: implement AdapterTrainingConfig for parameter management

---
+++ Train/train_config.py
@@ -0,0 +1,18 @@
+class TrainerConfig(object):
+  def __init__(self):
+    self.base_path = "./dataset_2/local_flan_v2_2"
+    self.base_model = "meta-llama/Llama-3.1-8B-Instruct",
+    self.model_type = "LLaMA",
+    self.data_path = full_path,
+    self.output_dir = "./lora_adapters_fine_tuned/6_2",
+    self.adapter_name = "lora",
+    self.wandb_project = "LoGo Adapters_5",
+    self.wandb_run_name = task,
+    self.wandb_watch = "false",  # options: false | gradients | all
+    self.wandb_log_model = "false",  # options: false | true
+    self.resume_from_checkpoint = None,  # either training checkpoint or final adapter
+    self.num_epochs = 10,
+    self.eval_step = 50,
+    self.save_step = 50,
+
+    
